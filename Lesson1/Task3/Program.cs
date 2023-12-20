5e9da80ef04bf89093404653c343000979f8a71f
rejey21 <100fak100dlmas@gmail.com>
2023-12-20 16:24:03 +0300

    Init commit

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 17;
+int b = 5;
+
+if (a > b){
+    Console.WriteLine(a);
+} else {
+    Console.WriteLine(b);
+}
---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿// int N = 9;
+// int i;
+
+// for (i=1;i<=N;i++){
+//     Console.WriteLine(i);
+// }
+
+//Second variant
+int N = 1000;
+int i = 1;
+
+while (i <= N)
+{
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}

2048eaafff79ff1220535bf0f1dedfab39eef416
rejey21 <100fak100dlmas@gmail.com>
2024-01-12 13:59:13 +0300

    add Lesson2:task2 and Lesson2:task3

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Task1
+int N = 10;
+
+int[] arr = new int[N];
+
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task2
+int n = 10;
+
+int[] array = {1,3,5,7,4,8,11,9,35,19};
+
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    i = i + 1;
+}

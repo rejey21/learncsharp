5c71a61ded6abfd9e69fc1a5b6e3b8699b4f8014
rejey21 <100fak100dlmas@gmail.com>
2024-01-12 14:48:50 +0300

    add Lesson2:task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,42 @@
+﻿int n = 5;
+
+int[] array = { 5, 2, 8, 1, 3};
+
+int i = 0;
+int max = array[0];
+
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+
+    i = i + 1;
+}
+
+Console.WriteLine(max);
+
+max = array[0];
+
+for(int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+
+Console.WriteLine(max);
+
+max = array[0];
+
+foreach(int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+
+Console.WriteLine(max);
